90deb5a7246fa87cf9e073f9add5a1359a765f4b
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-01 19:23:04 -0400

    Add prettier format

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5',
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
