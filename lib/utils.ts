d8986a2061621223275f78502b5c6114dbb2066d
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-02 00:47:22 -0400

    Update: getBaseUrl

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ import { type ClassValue, clsx } from 'clsx';
 import { cache } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export const getBaseUrl = cache(() => `http://localhost:3000`);
+export const getBaseUrl = cache(() => process.env.VERCEL_URL ?? 'http://localhost:3000');
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
