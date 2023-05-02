2fadeefbf04d5ab06a3ccb26ef804cfab76cc0bf
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-02 12:43:03 -0400

    test: parallel routes

--- app/@games/page.tsx
+++ app/@games/page.tsx
@@ -2,18 +2,20 @@ import GameCard from '@/components/ui/game-card';
 import { getBaseUrl } from '@/lib/utils';
 import type { Games } from '@/types';
 
-export default async function Home() {
+export const runtime = 'experimental-edge';
+
+export default async function GameContainer() {
   const {
     games: { results },
   }: Games = await fetch(`${getBaseUrl()}/api`).then((res) => res.json());
 
   return (
-    <main className='container'>
+    <section className='container'>
       <div className='grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4'>
         {results.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
       </div>
-    </main>
+    </section>
   );
 }
