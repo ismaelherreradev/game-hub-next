6b3d47fa53a5dad1856ac8b6815696c4367c9fa1
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-06 19:58:46 -0400

    Add caching headers to game pages

--- app/@games/game/[slug]/page.tsx
+++ app/@games/game/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { getBaseUrl } from '@/lib/utils';
 import { Games } from '@/types/games';
 
+export const runtime = 'edge';
+export const revalidate = 60;
+
 export default async function page({ params }: { params: { id: string } }) {
   const game = await fetch(`${getBaseUrl()}/game`).then((res) => res.json());
 
--- app/@games/page.tsx
+++ app/@games/page.tsx
@@ -3,6 +3,7 @@ import { getBaseUrl } from '@/lib/utils';
 import type { Games } from '@/types/games';
 
 export const runtime = 'edge';
+export const revalidate = 60;
 
 export default async function GameContainer() {
   const {
